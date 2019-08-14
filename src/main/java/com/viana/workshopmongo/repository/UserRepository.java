7406469d974eea78682b3d27ff531c606be1d022
TheusKira <matheusviana.tk@gmail.com>
2019-08-14 12:51:12 -0300

    CRUD Completo

---
+++ src/main/java/com/viana/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.viana.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.viana.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
---
+++ src/main/java/com/viana/workshopmongo/services/UserService.java
@@ -0,0 +1,68 @@
+package com.viana.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viana.workshopmongo.domain.User;
+import com.viana.workshopmongo.dto.UserDTO;
+import com.viana.workshopmongo.repository.UserRepository;
+import com.viana.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll(){
+		
+		return repo.findAll();
+		
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+	
+	public User insert(User obj) {
+		
+		return repo.insert(obj);
+		
+	}
+	
+	public void delete(String id) {
+		
+		findById(id);
+		
+		repo.deleteById(id);
+		
+	}
+	
+	public User update(User obj) {
+		
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+
+	private void updateData(User newObj, User obj) {
+		
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+		
+	}
+	
+}
